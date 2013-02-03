97f4fe3d9fdaa7249a3fce21512e8c494bb9f530
ignisf <bordjukov@gmail.com>
2013-02-03 21:38:05 +0200

    Parameters comparison and accessor implemented

--- yavor/parameters.rb
+++ yavor/parameters.rb
@@ -7,6 +7,8 @@ module Yavor
       include Enumerable
       extend Forwardable
 
+      attr_reader :params
+
       def initialize(*params)
         raise ArgumentError if params[0...-1].any? { |param| param.include? ' ' }
         @params = params
@@ -15,6 +17,10 @@ module Yavor
       def to_s
         @params.last.prepend(':') if @params.last and @params.last.include? ' '
         @params.join ' '
+      end
+
+      def ==(other)
+        params == other.params
       end
 
       def_delegator :@params, :[]
