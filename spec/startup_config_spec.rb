a2f61ef9073707d7f772554fd8ddaff6bd3b0555
Petko Bordjukov <bordjukov@gmail.com>
2014-01-04 22:54:15 +0200

    Rework the StartupOptions spec

--- spec/startup_config_spec.rb
+++ spec/startup_config_spec.rb
@@ -2,34 +2,15 @@ require 'lib/startup_config'
 
 module Yavor
   describe StartupConfig do
-    subject { StartupConfig.clone.instance }
-
-    it { should be_an OpenStruct }
-
-    it 'should be a singleton' do
-      subject.class.should respond_to :instance
-      subject.should eq subject.class.instance
-    end
-
-    it { should be_frozen }
-
-    describe 'initialization' do
-      before(:each) do
-        stub_const 'Yavor::StartupConfig::DEFAULTS', {foo: 'bar', stefi: 'kancho'}
-        stub_const 'ARGV', ['--lol', 'wut']
+    describe '::options' do
+      it 'should point to a default configuration file' do
+        StartupConfig.options[:config].should eq 'config.yml'
       end
 
-      it 'should assign default values' do        
-        subject.foo.should eq 'bar'
-        subject.stefi.should eq 'kancho'
+      it 'should parse command line options' do
+        stub_const 'ARGV', ['--config=kancho.yml']
+        StartupConfig.options[:config].should eq 'kancho.yml'
       end
-
-      it 'should parse command line arguments'
-    end
-
-    describe 'default values' do
-      its(:config_file) { should_not be_nil }
-      its(:daemonize) { should_not be_nil }
     end
   end
 end
